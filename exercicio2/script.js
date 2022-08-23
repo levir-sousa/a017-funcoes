6d53bbd04038a2f1e22adf932ebada92c9ed5e22
levir-sousa <levysouza2014@gmail.com>
2022-08-23 18:35:03 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,38 @@
+// // a)
+// function soma(num1,num2){
+//     console.log(num1+num2);
+// }
+
+// soma(1,2)
+
+// // b)
+// function boolean(num1,num2){
+//     if(num1 >= num2){
+//         console.log(true)
+//     }else{
+//         console.log(false)
+//     }
+// }
+
+// boolean(0,0)
+
+// // c)
+// function parOuImpar(num){
+//     if(num%2===0){
+//         console.log("Seu número é par");
+//     } else {
+//         console.log("Seu número é ímpar");
+//     }
+// }
+
+// parOuImpar(0)
+
+// // d)
+// const string= prompt("Fale uma frase")
+
+// const funcao = (funcao) => {
+//     console.log(`Tamanho da string: ${funcao.length} caracteres!`)
+//     console.log(`Versão com letras maiusculas da frase: ${funcao.toUpperCase()}`);
+// }
+
+// funcao(string)
